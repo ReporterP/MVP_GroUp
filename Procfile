365ab5c2c029201e246e7e47246aa9615b7f4928
Alex <alex_step04@mail.ru>
2022-06-07 12:52:01 +0300

    add heroku, bot, database and server files

---
+++ Procfile
@@ -0,0 +1,2 @@
+server: python ./server/server.py
+bot: python ./bot/bot.py
---
+++ server/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, abort, send_file, jsonify
+from werkzeug.utils import secure_filename
+from flask.helpers import make_response, send_from_directory
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__, static_folder="front/build", static_url_path="")
+
+@app.route('/', methods=['GET'])
+@cross_origin()
+def serve():
+    return send_from_directory(app.static_folder, "index.html")
+
+if __name__ == '__main__':
+    app.run(threaded=True)
