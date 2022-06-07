9858406d58fa8a788b1dd56054d7e51c426be7c4
Alex <alex_step04@mail.ru>
2022-06-07 13:20:11 +0300

    make better

--- server/server.py
+++ server/server.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask, request, abort, send_file, jsonify
 from werkzeug.utils import secure_filename
 from flask.helpers import make_response, send_from_directory
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_folder="front/build", static_url_path="")
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/', methods=['GET'])
 @cross_origin()
