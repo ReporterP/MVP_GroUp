9a87c5f65cda773776bda53507e6eb260b32e371
Marat Gevorkyan <marat2255a@gmail.com>
2022-09-24 21:31:31 +0300

    add roadmap in profile

--- frontend/src/components/profile/UI/Roadmap.jsx
+++ frontend/src/components/profile/UI/Roadmap.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from 'react'
+import RoadmapPoint from '../../../img/RoadmapPoint.svg'
+import EditButton from '../../../img/EditButtonIcon.svg'
+import PlusIcon from '../../../img/PlusIcon.svg'
 
 const Roadmap = () => {
+    const point = {
+        date: {dm: '10.05', y: '2022'},
+        name: 'Релиз приложения CoolApp'
+    }
+
+    let points = [];
+
+    for (let i = 0; i < 20; i++) {
+        points.push(point)
+    }
+
     return (
         <div className='roadmap'>
-            
+            <table>
+                {points.map(p => 
+                    <tr>
+                        <td>
+                            <img className='editButton' src={EditButton} alt='edit button' onClick={()=>{console.log('edit point')}} />
+                        </td>
+                        <td className='date'>
+                            {p.date.dm}
+                            <br />
+                            {p.date.y}
+                        </td>
+                        <td>
+                            <img className='roadmapPoint' src={RoadmapPoint} alt="roadmap point" />
+                        </td>
+                        <td className='name'>
+                            {p.name}
+                        </td>
+                    </tr>
+                )}
+            </table>
+
+            <div className='flatButton' onClick={()=>{console.log('add point')}}>
+                <img src={PlusIcon} alt="add point" />
+            </div>
         </div>
     );
 }
