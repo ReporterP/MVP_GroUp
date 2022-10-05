6db50fad642f690be0cf5c1ee3506278a493f48c
Marat Gevorkyan <marat2255a@gmail.com>
2022-10-05 03:02:36 +0300

    add popup for roadmap`s points creation

--- frontend/src/components/profile/UI/Roadmap.jsx
+++ frontend/src/components/profile/UI/Roadmap.jsx
@@ -3,7 +3,10 @@ import RoadmapPoint from '../../../img/RoadmapPoint.svg'
 import EditButton from '../../../img/EditButtonIcon.svg'
 import PlusIcon from '../../../img/PlusIcon.svg'
 
+
 const Roadmap = () => {
+    const [creationPopupShown, setCreationPopupShown] = React.useState(false);
+
     const point = {
         date: {dm: '10.05', y: '2022'},
         name: 'Релиз приложения CoolApp'
@@ -37,8 +40,23 @@ const Roadmap = () => {
                     </tr>
                 )}
             </table>
+            
+            {creationPopupShown ? (
+                <div className='creationPopup'>
+                    <input type='text' className='styledInput' placeholder='Название' />
+                    <input type='date' className='styledInput' placeholder='Дата' required pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" />
+                    <div className="flex flexSpaceBetween">
+                        <button className='voidButton' onClick={()=>setCreationPopupShown(false)}>
+                            Отменить
+                        </button>
+                        <button className='greenButton'>
+                            Сохранить
+                        </button>
+                    </div>
+                </div>
+            ) : ''}
 
-            <div className='flatButton' onClick={()=>{console.log('add point')}}>
+            <div className='flatButton' onClick={() => setCreationPopupShown(!creationPopupShown)}>
                 <img src={PlusIcon} alt="add point" />
             </div>
         </div>
