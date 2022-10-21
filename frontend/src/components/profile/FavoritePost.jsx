b88b9239fdf8198755fd0b1971fcddc8eeb35b06
Marat Gevorkyan <marat2255a@gmail.com>
2022-10-21 07:02:01 +0300

    Added using the Posts component

--- frontend/src/components/profile/FavoritePost.jsx
+++ frontend/src/components/profile/FavoritePost.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
 import PostImage from '../../img/PostImage.png'
-import Post from '../posts/Post';
+import Posts from '../posts/Posts';
 
 const FavoritePost = () => {
-  var card = {
-    status: ['#24D756', "Мироприятие"],
-    picture: PostImage,
-    name: "Мероприятие длинное название",
-    tags: [["онлайн", '#E6D268'], ["JavaScript", '#68E68B'], 
-    ["онлайн", '#E6D268'], ["JavaScript", '#68E68B'], ["онлайн", '#E6D268'], ["JavaScript", '#68E68B'], ["онлайн", '#E6D268'], ["JavaScript", '#68E68B']
-],
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum."
-}
+  // TODO: Fetch data from DB to `favPosts`
+  /* Fake data */
+  let favPosts = [];
+  for (let i = 0; i < 4; i++) {
+      favPosts.push(
+          {
+              status: ['#24D756', "Мероприятие"],
+              picture: Math.random() < 0.5 ? PostImage : '',
+              name: `Мероприятие №${i+1} длинное название`,
+              tags: [
+                  ["онлайн", '#E6D268'],
+                  ["JavaScript", '#68E68B'],
+                  ["онлайн", '#E6D268'],
+                  ["JavaScript", '#68E68B'],
+                  ["онлайн", '#E6D268'],
+                  ["JavaScript", '#68E68B'],
+                  ["онлайн", '#E6D268'],
+                  ["JavaScript", '#68E68B']
+              ],
+              description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+      )
+  }
+  /* Fake data */
+
   return (
     <div>
-      <Post
-      status={card.status} 
-      picture={card.picture}
-      name={card.name} 
-      tags={card.tags} 
-      description={card.description} />
-      <Post
-      status={card.status} 
-      picture={card.picture}
-      name={card.name} 
-      tags={card.tags} 
-      description={card.description} />
-      <Post
-      status={card.status} 
-      picture={card.picture}
-      name={card.name} 
-      tags={card.tags} 
-      description={card.description} />
+      <Posts cards={favPosts} withPaddingTop={false} />
     </div>
   );
 }
