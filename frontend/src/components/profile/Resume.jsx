80a709577dedff2b50c0e022c83ad72e1b38266b
Marat Gevorkyan <marat2255a@gmail.com>
2022-10-24 19:43:46 +0300

    Improve Resume component

--- frontend/src/components/profile/Resume.jsx
+++ frontend/src/components/profile/Resume.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import EditableAccordionsArea from './EditableAccordionsArea';
 
 const Resume = () => {
+  // TODO: create hardSkills, softSkills, workExperience consts and get data from DB here
+  const hardSkills = [
+    {
+      id: 1,
+      text: 'Python',
+      starsCount: 3,
+      description: 'Django, Flask, PyGame, FastAPI, Pandas, NumPy, Nimpa'
+    },
+    {
+      id: 2,
+      text: 'C#',
+      starsCount: 4,
+      description: 'ADO.NET, EntityFramework, .NET 6, WPF, UWP, MAUI, Xamarin'
+    },
+    {
+      id: 3,
+      text: 'Dart',
+      starsCount: 2,
+      description: 'Flutter'
+    }
+  ]
+
+  // TODO: set data here
   return (
-    <div>
-      Resume
+    <div id='resumeArea'>
+      <EditableAccordionsArea title={'Hard-skills'} color={'#68e68b'} accordions={hardSkills} />
+
+      <EditableAccordionsArea title={'Soft-skills'} color={'#ffbd70'} accordions={hardSkills} />
+
+      <EditableAccordionsArea title={'Опыт работы'} color={'#7c91ff'} accordions={hardSkills} />
     </div>
   );
 }
